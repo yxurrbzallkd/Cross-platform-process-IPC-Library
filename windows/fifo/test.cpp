8b1380b515b3f3b56bd928971f66611f1244feb2
yxurrbzallkd <kypybida@ucu.edu.ua>
2022-12-23 21:07:08 +0200

    removed trash

--- windows/fifo/test.cpp
+++ windows/fifo/test.cpp
@@ -2,9 +2,13 @@
 
 using namespace std;
 
+CHAR name[] = "\\\\.\\pipe\\Pipe";
+CHAR programPath[] = "./test.exe";
+
+BOOL CreateChildProcess(PROCESS_INFORMATION&, STARTUPINFO&);
+
 int main(int argc, char* argv[]) {
-	CHAR name[] = "\\\\.\\pipe\\Pipe";
-	CHAR programPath[] = "./test.exe";
+	
 	if (argc == 2) {
 		PROCESS_INFORMATION piProcInfo;
 		STARTUPINFO siStartInfo;
@@ -16,23 +20,16 @@ int main(int argc, char* argv[]) {
 		if (pipe.setup() != SUCCESS)
 			return -1;
 
-		BOOL bSuccess = CreateProcessA(NULL,
-									   programPath,	// command line
-									   NULL,		// process security attributes
-									   NULL,		// primary thread security attributes
-									   TRUE,		// handles are inherited
-									   0,			// creation flags
-									   NULL,		// use parent's environment
-									   NULL,		// use parent's current directory
-									   &siStartInfo,	// STARTUPINFO pointer
-									   &piProcInfo);	// receives PROCESS_INFORMATION
-		if (!bSuccess)
+		if (!CreateChildProcess(piProcInfo, siStartInfo))
 			return -1;
+
 		CHAR buf[1024];
 		DWORD dwRead;
 		pipe.read_fifo(buf, 1024, dwRead);
 		buf[dwRead] = '\0';
 		std::cout << buf << std::endl;
+		CloseHandle(piProcInfo.hThread);
+		CloseHandle(piProcInfo.hProcess);
 	} else {
 		// child
 		CHAR* hello = (char*)"hello world!";
@@ -44,4 +41,23 @@ int main(int argc, char* argv[]) {
 	}
 	return 0;
 }
+
+
+BOOL CreateChildProcess(PROCESS_INFORMATION& piProcInfo, STARTUPINFO& siStartInfo) {
+	ZeroMemory( &piProcInfo, sizeof(PROCESS_INFORMATION) );
+	ZeroMemory( &siStartInfo, sizeof(STARTUPINFO) );
+	siStartInfo.cb = sizeof(STARTUPINFO);
+	BOOL bSuccess = CreateProcessA(NULL,
+									   programPath,	// command line
+									   NULL,		// process security attributes
+									   NULL,		// primary thread security attributes
+									   TRUE,		// handles are inherited
+									   0,			// creation flags
+									   NULL,		// use parent's environment
+									   NULL,		// use parent's current directory
+									   &siStartInfo,	// STARTUPINFO pointer
+									   &piProcInfo);	// receives PROCESS_INFORMATION
+	return bSuccess;
+}
+
 
